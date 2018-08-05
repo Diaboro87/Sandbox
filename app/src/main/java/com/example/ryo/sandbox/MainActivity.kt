baba70a11a73de8a1e0a4ff76b1dd5ed73950a06
Ryo <diaboro77@gmail.com>
2018-08-05 17:09:27 +0900

    [mod] 単純な画面遷移を実装

--- app/src/main/java/com/example/ryo/sandbox/MainActivity.kt
+++ app/src/main/java/com/example/ryo/sandbox/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.ryo.sandbox
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.login) as Button
+        button.setOnClickListener { view ->
+            val intent = Intent(this, InfoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
